e1f34bfcbe9ca444a07c9b1b4edbdef0219ad0ae
Nasiru Murana <murannasir22@gmail.com>
2022-10-13 13:01:49 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printout text
+ *
+ * Return: Always 1 (imran)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of are is useful\" - Dota Korpar, 2015-10-19\n", 59);
+	return(1);
+}
