9da8003cee229b981abe0913cc0c90bf776af1f8
Nasiru Murana <murannasir22@gmail.com>
2022-10-18 04:24:22 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar by a new line
+ * Return: Alwasy 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
