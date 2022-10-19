11b260e90d3c064394cda38e66eafea06c62811f
Nasiru Murana <murannasir22@gmail.com>
2022-10-19 07:39:22 -0700

    tsk 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an interger
+ * @t: the interget to be computed
+ * Return: the absolute value
+ */
+
+int _abs(int t)
+{
+	if (t > 0)
+		return (t);
+	else
+		return (-t);
+}
