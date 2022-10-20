d9ab935fcb617ec0ea551d2ec061220ea2ffbbe3
Nasiru Murana <murannasir22@gmail.com>
2022-10-20 16:09:32 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the n times table
+ * @n: the number
+ * Return: alwasy 0
+ */
+
+void print_times_table(int n)
+{
+	if (n > 15)
+	{
+	else if	(n < 0)
+
+		_putchar('\n');
+	return (0);
+	}
+}
