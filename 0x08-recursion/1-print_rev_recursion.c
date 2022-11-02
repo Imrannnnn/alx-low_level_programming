7634ad41915a1897fc8d33345ff2ecd442599018
Nasiru Murana <murannasir22@gmail.com>
2022-11-02 15:03:58 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _prints_rev_recursion – prints a string in reverse.
+ * @s: string to be printed
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+
+}
+
