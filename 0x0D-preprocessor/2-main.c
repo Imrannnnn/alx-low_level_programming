856d7d60306243b281a136162069486d0f59c7ca
Nasiru Murana <murannasir22@gmail.com>
2022-11-14 11:38:17 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - printing file name using the predefined macros
+  *
+  *Return: always 0
+  */
+
+int main(void)
+{
+	printf("%s/n", __FILE__);
+	return (0);
+}
+
